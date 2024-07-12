f9083a648562532c63432184ccac494103a2c98b
Alan Davis <155010643+akdavis83@users.noreply.github.com>
2024-07-12 18:35:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -64,11 +64,11 @@ class Website3DDemo {
     this._controls.update();
 
     // this._LoadAnimatedModelAndPlay(
-    //     '/resources/zombie/', 'mremireh_o_desbiens.fbx',
+    //     './resources/zombie/', 'mremireh_o_desbiens.fbx',
     //     'Silly Dancing.fbx', new THREE.Vector3(0, 0, 0));
 
     this._LoadAnimatedModelAndPlay(
-        '/resources/zombie/', 'mremireh_o_desbiens.fbx',
+        './resources/zombie/', 'mremireh_o_desbiens.fbx',
         'Silly Dancing.fbx', new THREE.Vector3(-20, 0, -20));
 
     // this._LoadAnimatedModelAndPlay(
